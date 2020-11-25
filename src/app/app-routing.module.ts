579a55eb8644f6ca4dedf76b71b6c9387f72f9a8
usmansaleh <usmansaleh910@gmail.com>
2020-11-25 17:10:15 +0500

    OMNI app solutions work assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  	{
+		path : '',
+		pathMatch : 'full',
+		component : MovieListComponent
+	},
+	{
+		path : 'detail',
+		pathMatch : 'full',
+		component : MovieDetailComponent
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { FilterPipe } from './filter.pipe';
+import { UtilityService } from './utility.service';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieDetailComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UtilityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
